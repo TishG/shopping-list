80e6563d205b28ff6b02fe3391f76bc055193efe
Tish <latisha.griffiths@programmer.net>
2019-06-22 18:39:24 -0500

    edited the code for serving static assets

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = require('path');
-
+const path = require("path");
 
 const items = require("./routes/api/items");
 //bodyParser middleware
@@ -22,13 +21,18 @@ mongoose
 app.use("/api/items", items);
 
 //Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // })
+  app.get(/^\/(?!api).*/, (req, res) => {
+    // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  console.log("Serving React App...");
 }
 
 //Server
