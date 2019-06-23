082e2240aa6a058f1821e753303b175fcc2bd4af
Tish <latisha.griffiths@programmer.net>
2019-06-23 16:18:20 -0500

    added empty favicon link to try and fix favicon.ico error

--- server.js
+++ server.js
@@ -25,12 +25,8 @@ if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // })
-  app.get(/^\/(?!api).*/, (req, res) => {
-    // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
   console.log("Serving React App...");
 }
